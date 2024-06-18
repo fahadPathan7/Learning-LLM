be3a472ebbb8dd771cf5a261691adb97218ca65c
Fahad Pathan <fahadpathan56@gmail.com>
2024-06-19 00:25:19 +0600

    add: basic restaurant name generator project completed

--- RestaurantNameGenerator/main.py
+++ RestaurantNameGenerator/main.py
@@ -1,5 +1,26 @@
+# external imports
 import streamlit as st
+import warnings
 
+# internal imports
+import langchain_helper
+
+# ignore deprecation warnings
+warnings.filterwarnings('ignore', category=DeprecationWarning)
+
+# setting title
 st.title("Restaurant Name Generator")
 
-st.sidebar.selectbox("pick a cuisine", ("Indian", "Italic", "Arabic"))
+# sidebar to select cuisine type
+cuisine = st.sidebar.selectbox("pick a cuisine",
+                               ("Bangladeshi", "Indian", "Italian",
+                                "Arabian", "Pakistani", "Chinese", "Lebanese"))
+
+# generate restaurant name and menu items
+if cuisine:
+    response = langchain_helper.generate_restaurant_name_and_items(cuisine)
+    st.header(response['restaurant_name'].strip())
+    menu_items = response['menu_items'].strip().split(",")
+    st.write("**Suggested Menu Items**")
+    for item in menu_items:
+        st.write("-", item)
---
+++ RestaurantNameGenerator/secret_key.py
@@ -0,0 +1,1 @@
+openapi_key = 'sk-proj-XrCY9YQ5P521Dn1Nbm7GT3BlbkFJjUzEoas1SZNJBp8KjEj8'
